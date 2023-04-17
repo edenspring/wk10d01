0c0c7803907a30cab84b881a71ccd63c402e07ac
Bill Adams <vikingbill@gmail.com>
2023-04-17 13:21:08 -0400

    initial commit

---
+++ linkedLists.js
@@ -0,0 +1,8 @@
+/*
+What is this list and how do I link it? Or really, why?
+
+When we learned about Arrays, we learned that an array is a sequence of 
+elements of the same type stored in a contiguous block of memory. We also
+learned that things are a bit whacky in Javascript. We've got dynamic arrays
+that overallocatet memory so
+*/
